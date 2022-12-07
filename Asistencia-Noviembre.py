28ba8a66a08466ef1d3b1e0eac7555e70dc94a40
GiuntaP <113065894+GiuntaP@users.noreply.github.com>
2022-12-07 17:26:51 -0300

    Update Asistencia-Noviembre.py

--- Asistencia-Noviembre.py
+++ Asistencia-Noviembre.py
@@ -85,6 +85,38 @@ bici = Bicicleta('Azul', 2, 'Urbana')
 print(bici)
 
 #######################################
-Alumno/a : 
+Alumno/a :Giunta Pilar
 #######################################
 
+class Producto:
+    contador_producto = 0 #Variable de clase
+
+    def __init__(self, nombre, precio):
+        Producto.contador_productos += 1 #Aumento para la variable de clase
+        self._id_producto = Producto.contador_productos #Asignación desde la variable de clase
+        self._nombre = nombre
+        self._precio = precio
+    # Métodos setters and getters
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
+
+    @property
+    def precio(self):
+        return self._precio
+
+    @precio.setter
+    def precio(self, precio):
+        self._precio =precio
+    #Sobre escribimos el método srt
+    def __str__(self):
+      return f'Id producto: {self._id_producto}, Nombre: {self._nombre}, Precio: {self._precio}'
+
+
+    if __name__ == '__main__': #Solo será visible si la prueba se ejecuta desde aqui
+        producto1 = Producto('Camiseta', 100.00)
+        producto2 = Producto('Pantalon', 150.00)
