a91eb58044a706e70739bea5717cf3139c26b65d
Octavio Veron <113061341+MoriMemento@users.noreply.github.com>
2022-12-07 19:59:39 -0300

    Update Asistencia-Noviembre.py

--- Asistencia-Noviembre.py
+++ Asistencia-Noviembre.py
@@ -120,3 +120,34 @@ class Producto:
     if __name__ == '__main__': #Solo será visible si la prueba se ejecuta desde aqui
         producto1 = Producto('Camiseta', 100.00)
         producto2 = Producto('Pantalon', 150.00)
+
+#######################################
+Alumno/a : Octavio Veron
+#######################################
+
+class Persona:
+    contador_personas = 0
+
+    @classmethod
+    def siguiente_valor(cls):
+        cls.contador_personas += 1
+        return cls.contador_personas
+
+    def __init__(self, nombre, edad):
+        self.id_persona = Persona.siguiente_valor()
+        self.nombre = nombre
+        self.edad = edad
+
+    def __str__(self):
+        return f'Persona [{self.id_persona} = {self.nombre} {self.edad}]'
+
+persona1 = Persona("Octavio", 21)
+print(persona1)
+persona2 = Persona("Daniel", 25)
+print(persona2)
+persona3 = Persona("Carlos", 47)
+print(persona3)
+Persona.siguiente_valor()
+persona4 = Persona("Jose", 28)
+print(persona4)
+print(f"Valor contador personas: {Persona.contador_personas}")
