5436ebf05c3bc6df740e71f887908b15561de7af
Dani Dasilva <113066584+Dvni22@users.noreply.github.com>
2022-12-06 21:34:38 -0300

    Update Asistencia-Noviembre.py

--- Asistencia-Noviembre.py
+++ Asistencia-Noviembre.py
@@ -25,3 +25,66 @@ print(persona3)
 print(f'Valor de contador: {Persona.contador_personas}')
 
 #######################################
+Alumno/a : Daniel Dasilva
+#######################################
+
+class Vehiculo:
+    '''
+    Definir una calse padre llamada Vehiculo y dos clases hijas llamadas
+    Auto y Bicicleta, las cuales heredan de la calase padre Vehiculo. La clase
+    padre debe tener los siguientes atributos y métodos:
+    Vehiculo (clase padre)
+    -Atributos(Color, ruedas)
+    -Metodos(__init__(color, ruedas,) y __str__())
+
+    Auto(clase hija de Vehiculo)
+    -Atributos(velocidad (km/hr))
+    -Metodos(__init__(Color, ruedas, velocidad) y __str__())
+
+    Bicicleta(clase hija de Vehiculo)
+    -Atributos(tipo(urbana/montaña/etc.)
+    -Metodos(__init__(Color, ruedas, tipo) y __str__())
+    Crear un objeto de cada clase
+    '''
+
+    def __init__(self, color, ruedas):
+        self.color = color
+        self.ruedas = ruedas
+
+    def __str__(self):
+        return 'Color: ' + self.color + ', Ruedas: ' + str(self.ruedas)
+
+
+class Auto(Vehiculo):
+    def __init__(self, color, ruedas, velocidad):
+        super().__init__(color, ruedas)
+        self.velocidad = velocidad
+
+    def __str__(self):
+        return super().__str__()+', Velocidad(km/hr): '+ str(self.velocidad)
+
+class Bicicleta(Vehiculo):
+    def __init__(self, color, ruedas, tipo):
+        super().__init__(color, ruedas)
+        self.tipo = tipo
+
+    def __str__(self):
+        return super().__str__() + ', Tipo: ' + self.tipo
+
+
+# Primer objeto de la clase padre Vehiculo
+vehiculo = Vehiculo('Blanco', 4)
+print(vehiculo)
+
+# Segundo objeto, pero ahora de la clase Auto
+auto = Auto('Amarillo', 4, 120)
+print(auto)
+
+# Tercer objeto, el último de la clase Bicilcleta
+bici = Bicicleta('Azul', 2, 'Urbana')
+print(bici)
+
+#######################################
+Alumno/a : 
+#######################################
+
