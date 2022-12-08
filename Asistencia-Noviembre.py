55a5c7f492a51e3b7841a9b19402eb917664229f
Franco Leone <112661380+franco8787@users.noreply.github.com>
2022-12-07 21:46:39 -0300

    Update Asistencia-Noviembre.py

--- Asistencia-Noviembre.py
+++ Asistencia-Noviembre.py
@@ -121,6 +121,35 @@ class Producto:
         producto1 = Producto('Camiseta', 100.00)
         producto2 = Producto('Pantalon', 150.00)
 
+
+
+#######################################
+Alumno/a : Franco Leone
+#######################################
+
+class MiClase:
+    # variable de clase, este atributo dara a cada objeto el mismo valor
+    variable_clase = "Esta es una variable de clase"
+
+    def __init__(self, variable_instancia): # La variable es instancia, da diferentes valores
+        self.variable_instancia = variable_instancia
+
+
+print(MiClase.variable_clase)
+miClase1 = MiClase("Esta es una variable de instancia")
+print(miClase1.variable_clase)
+print(miClase1.variable_instancia)
+miClase2 = MiClase("Esta es otra prueba de variable de instancia")
+print(miClase2.variable_instancia)
+print(miClase2.variable_clase)
+
+MiClase.variable_clase2 = "Valor de variable de clase 2"
+print(MiClase.variable_clase2)
+print(miClase1.variable_clase2)
+print(miClase2.variable_clase2)
+
+
+
 #######################################
 Alumno/a : Octavio Veron
 #######################################
