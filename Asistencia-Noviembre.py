dddd35e3333c752977a3fad57d3be882d3b45545
Gisela Fernández <113073076+GiseF@users.noreply.github.com>
2022-12-09 20:11:55 -0300

    Update Asistencia-Noviembre.py

--- Asistencia-Noviembre.py
+++ Asistencia-Noviembre.py
@@ -180,3 +180,71 @@ Persona.siguiente_valor()
 persona4 = Persona("Jose", 28)
 print(persona4)
 print(f"Valor contador personas: {Persona.contador_personas}")
+
+
+
+#######################################
+Alumno/a : Fernandez Gisela
+#######################################
+
+
+
+
+'''
+Definir una clase padre llamada vehiculo y dos clases hijas llamadas
+Auto y Bicicleta, las cuales heredan de la clase padre vehiculo. La clase
+padre debe tener los siguientes atributos y metodos:
+Vehiculo ( clase padre)
+-Atributos(color, ruedas)
+-Métodos(__init__(color, rueda) y __str__())
+
+Auto( clase hija de vehiculo)
+-Atributos(velocidad (km/hr))
+-Metodos(__init__(color, ruedas, velocidad) y __str())
+
+Bicicleta(clase hija de vehiculo)
+-Atributos( tipo(urbana/montaña/ etc.)
+-Métodos( __init__(color, ruedas, tipo) y __srt__()
+
+Crear un objeto de cada clase
+'''
+
+
+class Vehiculo:
+    def __init__(self, color, ruedas):
+        self.color = color
+        self.ruedas = ruedas
+
+    def __str__(self):
+        return 'Color:   ' + self.color + ',  Ruedas:  ' + str(self.ruedas)
+
+
+class Auto(Vehiculo):
+    def __init__(self, color, ruedas, velocidad):
+        super().__init__(color, ruedas)
+        self.velocidad = velocidad
+
+    def __str__(self):
+        return super().__str__() + ', Velocidad(km/hr):' + str(self.velocidad)
+
+
+class Bicicleta(Vehiculo):
+    def __init__(self, color, ruedas, tipo):
+        super().__init__(color, ruedas)
+        self.tipo = tipo
+
+    def __str__(self):
+        return super().__str__()+ ', Tipo: ' + self.tipo
+
+
+# Primer objeto de la clase vehiculo
+vehiculo = Vehiculo('Blanco', 4)
+print(vehiculo)
+
+# Segundo objeto de la clase padre vehiculo
+auto = Auto('Amarillo', 4, 120)
+print(auto)
+
+# Tercer objeto, el ultimo de la clase Bicicleta
+bici = Bicicleta('Azul', 2, 'Urbana')
+print(bici)
